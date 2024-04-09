9a1f14c2f5aa012715b4ea979373737beea46e86
Opticpeak <opticpeak@gmail.com>
2024-04-09 17:33:31 +0200

    done Task3 hEx01

--- src/main/scala/hEx01/Task2.scala
+++ src/main/scala/hEx01/Task2.scala
@@ -3,8 +3,8 @@ package hEx01
 def reverse[A](l: List[A]): List[A] = {
   def reverseHelper(list: List[A], accum: List[A]): List[A] = {
     list match
-      case head :: tail => reverseHelper(tail, head :: accum)
       case Nil => accum
+      case head :: tail => reverseHelper(tail, head :: accum)
   }
   reverseHelper(l, List())
 }
--- src/main/scala/hEx01/Task3.scala
+++ src/main/scala/hEx01/Task3.scala
@@ -1,3 +1,10 @@
 package hEx01
 
-def flatten[A](xss : List[List[A]]) : List[A] = ???
+def flatten[A](xss : List[List[A]]) : List[A] = {
+  def flattenHelper(remainder: List[List[A]], xs: List[A]): List[A] = {
+    remainder match
+      case Nil => xs
+      case head :: tail => flattenHelper(tail, xs ++ head)
+  }
+  flattenHelper(xss, List())
+}
