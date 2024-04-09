740e49c793d52259371bbe7616ce6a2c1b29c61e
Opticpeak <opticpeak@gmail.com>
2024-04-09 16:49:28 +0200

    done Task1 hEx01

--- src/main/scala/hEx01/Task1.scala
+++ src/main/scala/hEx01/Task1.scala
@@ -5,6 +5,12 @@ enum Nat:
   case Succ(n: Nat)
 import Nat.*
 
-def add(n: Nat, m: Nat): Nat = ???
-def mult(n: Nat, m: Nat): Nat = ???
+def add(n: Nat, m: Nat): Nat = n match
+  case Zero() => m
+  case Succ(x) => add(x, Succ(m))
+  
+def mult(n: Nat, m: Nat): Nat = n match
+  case Zero() => Zero()
+  case Succ(Zero()) => m
+  case Succ(x) => mult(x, add(m, m))
 
