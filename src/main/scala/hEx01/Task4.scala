87da904cf2f65fd6215226319778c43a393e127b
Opticpeak <opticpeak@gmail.com>
2024-04-09 17:49:04 +0200

    done Task4 hEx01

--- src/main/scala/hEx01/Task4.scala
+++ src/main/scala/hEx01/Task4.scala
@@ -1,3 +1,11 @@
 package hEx01
 
-def merge[A, B](m1: Map[A, B], m2: Map[A, B]): Map[A, B] = ???
+import hEx02.Exp.True
+
+def merge[A, B](m1: Map[A, B], m2: Map[A, B]): Map[A, B] = {
+  var resultMap = m1
+  val keys = m2.keys
+  keys.foreach(key => if(m1.contains(key)) sys.error("redundant keys occurred"))
+  keys.foreach(key => resultMap = resultMap + (key -> m2.apply(key)))
+  return resultMap
+}
