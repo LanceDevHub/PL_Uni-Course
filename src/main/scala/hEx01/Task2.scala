91f25ca8991bbec7d61dc47805bb71a9013dd451
Opticpeak <opticpeak@gmail.com>
2024-04-09 17:05:57 +0200

    done Task1 hEx01

--- src/main/scala/hEx01/Task2.scala
+++ src/main/scala/hEx01/Task2.scala
@@ -1,3 +1,11 @@
 package hEx01
 
-def reverse[A](l: List[A]): List[A] = ???
+def reverse[A](l: List[A]): List[A] = {
+  def reverseHelper(list: List[A], accum: List[A]): List[A] = {
+    list match
+      case head :: tail => reverseHelper(tail, head :: accum)
+      case Nil => accum
+  }
+  reverseHelper(l, List())
+}
+
