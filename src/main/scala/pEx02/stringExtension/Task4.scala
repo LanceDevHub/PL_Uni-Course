10e283cc98d9e4889af5d2ea36a86e2300b6bc17
Opticpeak <opticpeak@gmail.com>
2024-04-11 15:59:32 +0200

    done task4 pEx02 desugar-function.

--- src/main/scala/pEx02/stringExtension/Task4.scala
+++ src/main/scala/pEx02/stringExtension/Task4.scala
@@ -5,58 +5,36 @@ import util.*
 
 import Exp.*
 
-def parse(se: SExp): Ext = se match
-  case SExp.List(List(SExp.Sym("str"), SExp.Sym(str))) => Ext.Str(str)
-  case SExp.List(List(SExp.Sym("strEquals"), str1, str2)) => Ext.StrEquals(parse(str1), parse(str2))
-  case SExp.List(List(SExp.Sym("charAt"), str, index)) => Ext.CharAt(parse(str), parse(index))
-  case SExp.List(List(SExp.Sym("head"), str)) => Ext.Head(parse(str))
-  case SExp.List(List(SExp.Sym("startsWith"), str, firstLetter)) => Ext.StartsWith(parse(str), parse(firstLetter))
-  case SExp.List(List(SExp.Sym("endsWith"), str, lastLetter)) => Ext.EndsWith(parse(str), parse(lastLetter))
-  case SExp.List(List(SExp.Sym("concat"), str1, str2)) => Ext.Concat(parse(str1), parse(str2))
-  case SExp.List(List(SExp.Sym("length"), str)) => Ext.Length(parse(str))
+def parse(se: SExp): Ext = ???
 
-  case SExp.Num(n) => Ext.Num(n)
-  case SExp.Sym("true") => Ext.True()
-  case SExp.Sym("false") => Ext.False()
-  case SExp.List(List(SExp.Sym("and"), lhs, rhs)) => Ext.And(parse(lhs), parse(rhs))
-  case SExp.List(List(SExp.Sym("or"), lhs, rhs)) => Ext.Or(parse(lhs), parse(rhs))
-  case SExp.List(List(SExp.Sym("not"), expr)) => Ext.Not(parse(expr))
-  case SExp.List(List(SExp.Sym("<=>"), lhs, rhs)) => Ext.BiImplication(parse(lhs), parse(rhs))
-  case SExp.List(List(SExp.Sym("=>"), lhs, rhs)) => Ext.Implication(parse(lhs), parse(rhs))
-  case SExp.List(List(SExp.Sym("if"), cond, thenBranch, elseBranch)) =>
-    Ext.If(parse(cond), parse(thenBranch), parse(elseBranch))
-  case SExp.List(List(SExp.Sym("+"), lhs, rhs)) => Ext.Plus(parse(lhs), parse(rhs))
-  case SExp.List(List(SExp.Sym("*"), lhs, rhs)) => Ext.Mult(parse(lhs), parse(rhs))
-
-  case _ => sys.error(s"$se could not be parsed")
 
 
 def desugar(expr: Ext): Exp = expr match
-  case Ext.Str(str) => Exp.Str(str)
-  case Ext.StrEquals(str1, str2) => Exp.StrEquals(desugar(str1), desugar(str2))
-  case Ext.CharAt(str, index) => Exp.CharAt(desugar(str), desugar(index))
-  case Ext.Head(str) => Exp.CharAt(desugar(str), Exp.Num(1))
-  case Ext.StartsWith(str, firstLetter) => Exp.StrEquals(desugar(Ext.Head(str)), desugar(firstLetter))
-  case Ext.EndsWith(str, lastLetter) =>
-    val strExp = desugar(str)
-    Exp.StrEquals(
-      Exp.CharAt(strExp, Exp.Length(strExp)),
-      desugar(lastLetter))
-  case Ext.Concat(str1, str2) => Exp.Concat(desugar(str1), desugar(str2))
-  case Ext.Length(str) => Exp.Length(desugar(str))
-
   case Ext.True() => True()
   case Ext.False() => False()
-  case Ext.Not(e) => Not(desugar(e))
+  case Ext.Not(expr) => Not(desugar(expr))
   case Ext.And(lhs, rhs) => And(desugar(lhs), desugar(rhs))
   case Ext.Or(lhs, rhs) => Or(desugar(lhs), desugar(rhs))
-  case Ext.If(cond, thenExpr, elseExpr) =>
-    If(desugar(cond), desugar(thenExpr), desugar(elseExpr))
+  case Ext.If(cond, thenExpr, elseExpr) => If(desugar(cond), desugar(thenExpr), desugar(elseExpr))
+  case Ext.Implication(lhs, rhs) => Or(Not(desugar(lhs)), desugar(rhs))
+  case Ext.BiImplication(lhs, rhs) => Or(And(Not(desugar(lhs)), Not(desugar(rhs))), And(desugar(lhs), desugar(rhs)))
   case Ext.Num(n) => Num(n)
   case Ext.Plus(lhs, rhs) => Plus(desugar(lhs), desugar(rhs))
   case Ext.Mult(lhs, rhs) => Mult(desugar(lhs), desugar(rhs))
-  case Ext.Implication(lhs, rhs) => Or(Not(desugar(lhs)), desugar(rhs))
-  case Ext.BiImplication(lhs, rhs) => Or(And(Not(desugar(lhs)), Not(desugar(rhs))), And(desugar(lhs), desugar(rhs)))
+
+  // string operations
+  case Ext.Str(s) => Str(s)
+  case Ext.StrEquals(str1, str2) => StrEquals(desugar(str1), desugar(str2))
+  case Ext.CharAt(str, index) => CharAt(desugar(str), desugar(index))
+  case Ext.Head(str) => CharAt(desugar(str), Num(1))
+  case Ext.StartsWith(str, firstLetter) => StrEquals(desugar(Ext.Head(str)), desugar(firstLetter))
+  case Ext.EndsWith(str, lastLetter) =>
+    val strExpr = desugar(str)
+    StrEquals(CharAt(strExpr, Length(strExpr)), desugar(lastLetter))
+  case Ext.Concat(str1, str2) => Concat(desugar(str1), desugar(str2))
+  case Ext.Length(str) => Length(desugar(str))
+
+
 
 enum Nat:
   case Zero()
@@ -67,68 +45,18 @@ enum Value:
   case Bool(b: Boolean)
   case Str(s: String)
 
-  def str: String = this match
-    case Str(s) => s
-    case _ => sys.error(s"$this should be a Value.String, but isn't")
-
   def int: Int = this match
-    case Num(nat) => peanoToInt(nat)
-    case _ => sys.error(s"$this should be a Value.String, but isn't")
+    case Num(n: Nat) => peanoToInt(n)
+    case e => sys.error(s"cant convert $e(value) to int.")
 
-def interp(expr: Exp): Value = expr match
-  case Str(str) => Value.Str(str)
-  case StrEquals(str1, str2) => Value.Bool(interp(str1).str == interp(str2).str)
-  case CharAt(str, index) => Value.Str(interp(str).str
-    .charAt(interp(index).int - 1).toString)
-  case Concat(str1, str2) => Value.Str(interp(str1).str.concat(interp(str2).str))
-  case Length(str) => Value.Num(intToPeano(interp(str).str.length))
+  def str: String = this match
+    case Str(str: String) => str
+    case e => sys.error(s"cant convert $e(value) to string.")
 
 
-  case Num(n) => Value.Num(intToPeano(n))
-  case True() => Value.Bool(true)
-  case False() => Value.Bool(false)
-  case Not(e) => interp(e) match {
-    case Value.Bool(true) => Value.Bool(false)
-    case Value.Bool(false) => Value.Bool(true)
-    case ev => sys.error("not expected a boolean value, but got " + ev)
-  }
-  case And(lhs, rhs) => interp(lhs) match {
-    case Value.Bool(false) => Value.Bool(false)
-    case Value.Bool(lhv) => interp(rhs) match {
-      case Value.Bool(true) => Value.Bool(true)
-      case Value.Bool(false) => Value.Bool(false)
-      case rhv => sys.error("and expected a boolean value, but got " + rhv)
-    }
-    case lhv => sys.error("and expected a boolean value, but got " + lhv)
-  }
-  case Or(lhs, rhs) => interp(lhs) match {
-    case Value.Bool(true) => Value.Bool(true)
-    case Value.Bool(lhv) => interp(rhs) match {
-      case Value.Bool(true) => Value.Bool(true)
-      case Value.Bool(false) => Value.Bool(false)
-      case rhv => sys.error("and expected a boolean value, but got " + rhv)
-    }
-    case lhv => sys.error("and expected a boolean value, but got " + lhv)
-  }
-  case If(cond, thenExpr, elseExpr) => interp(cond) match {
-    case Value.Bool(true) => interp(thenExpr)
-    case Value.Bool(false) => interp(elseExpr)
-    case condv => sys.error("if expected a boolean value for cond, but got " + condv)
-  }
-  case Plus(lhs, rhs) => interp(lhs) match {
-    case Value.Num(lhv) => interp(rhs) match {
-      case Value.Num(rhv) => Value.Num(add(lhv, rhv))
-      case rhv => sys.error("+ expected a number value, but got " + rhv)
-    }
-    case lhv => sys.error("+ expected a number value, but got " + lhv)
-  }
-  case Mult(lhs, rhs) => interp(lhs) match {
-    case Value.Num(lhv) => interp(rhs) match {
-      case Value.Num(rhv) => Value.Num(mult(lhv, rhv))
-      case rhv => sys.error("* expected a number value, but got " + rhv)
-    }
-    case lhv => sys.error("* expected a number value, but got " + lhv)
-  }
+
+
+def interp(expr: Exp): Value = ???
 
 
 import Nat.*
@@ -141,12 +69,13 @@ def mult(n: Nat, m: Nat): Nat = n match
   case Succ(n1) => add(mult(m, n1), m)
 
 // The pre condition n >= 0 has to hold
+
 def intToPeano(n: Int): Nat = n match
   case 0 => Zero()
-  case _ => Succ(intToPeano(n - 1))
+  case _ => Succ(intToPeano(n-1))
 
-def peanoToInt(nat: Nat): Int = nat match {
-  case Nat.Zero() => 0
-  case Nat.Succ(n) => 1 + peanoToInt(n)
-}
+
+def peanoToInt(nat: Nat): Int = nat match
+  case Zero() => 0
+  case Succ(x) => 1 + peanoToInt(x)
 
--- src/test/scala/pEx02/stringExtension/Task4Test.scala
+++ src/test/scala/pEx02/stringExtension/Task4Test.scala
@@ -4,9 +4,160 @@ import pEx02.stringExtension.Exp.*
 import org.scalatest.funsuite.AnyFunSuite
 import util.*
 import util.SExpParser.parseSExp
+import Nat.*
 
 class Task4Test extends AnyFunSuite:
+
+  val zero: Nat = Zero()
+  val one: Nat = Succ(zero)
+  val two: Nat = Succ(one)
+  val three: Nat = Succ(two)
+  val four: Nat = Succ(three)
+  val five: Nat = Succ(four)
+  val six: Nat = Succ(five)
+  val seven: Nat = Succ(six)
   def runProg(str: String): Value = interp(desugar(parse(parseSExp(str))))
 
-  // TODO test your code
-  
+
+  // some functions of conversion
+  test("testIntToPeano") {
+    assertResult(
+      seven
+    )(
+      intToPeano(7)
+    )
+  }
+
+  test("testIntToPeano2") {
+    assertResult(
+      zero
+    )(
+      intToPeano(0)
+    )
+  }
+
+  test("testPeanoToInt") {
+    assertResult(
+      7
+    )(
+      peanoToInt(seven)
+    )
+  }
+
+  test("testPeanoToInt2") {
+    assertResult(
+      0
+    )(
+      peanoToInt(zero)
+    )
+  }
+
+  test("testValueInt") {
+    assertResult(
+      7
+    )(
+      Value.Num(seven).int
+    )
+  }
+
+  test("testValueStr") {
+    assertResult(
+      "HelloWorld"
+    )(
+      Value.Str("HelloWorld").str
+    )
+  }
+
+
+  // desugar tests
+  test("testTrue") {
+    assertResult(
+      True()
+    )(
+      desugar(Ext.True())
+    )
+  }
+
+    test("testFalse") {
+      assertResult(
+        False()
+      )(
+        desugar(Ext.False())
+      )
+    }
+
+    test("testNot") {
+      assertResult(
+        Not(True())
+      )(
+        desugar(Ext.Not(Ext.True()))
+      )
+    }
+
+        // desugar string-operations
+
+    test("testStr") {
+      assertResult(
+        Str("Hello")
+      )(
+        desugar(Ext.Str("Hello"))
+      )
+    }
+
+    test("testStrEquals") {
+      assertResult(
+        StrEquals(Str("Hello"), Str("Hallo"))
+      )(
+        desugar(Ext.StrEquals(Ext.Str("Hello"), Ext.Str("Hallo")))
+      )
+    }
+
+    test("testCharAt") {
+      assertResult(
+        CharAt(Str("Hello"), Num(5))
+      )(
+        desugar(Ext.CharAt(Ext.Str("Hello"), Ext.Num(5)))
+      )
+    }
+
+  test("testHead") {
+    assertResult(
+      CharAt(Str("hello"), Num(1))
+    )(
+      desugar(Ext.Head(Ext.Str("hello")))
+    )
+  }
+
+  test("testStartsWith") {
+    assertResult(
+      StrEquals(CharAt(Str("hello"), Num(1)), Str("h"))
+    )(
+      desugar(Ext.StartsWith(Ext.Str("hello"), Ext.Str("h")))
+    )
+  }
+
+  test("testEndsWith") {
+    assertResult(
+      StrEquals(CharAt(Str("hello"), Length(Str("hello"))), Str("l"))
+    )(
+      desugar(Ext.EndsWith(Ext.Str("hello"), Ext.Str("l")))
+    )
+  }
+
+  test("testConcat") {
+    assertResult(
+      Concat(Str("hello"), Str(" world"))
+    )(
+      desugar(Ext.Concat(Ext.Str("hello"), Ext.Str(" world")))
+    )
+  }
+
+  test("testLength") {
+    assertResult(
+      Length(Str("hello"))
+    )(
+      desugar(Ext.Length(Ext.Str("hello")))
+    )
+  }
+
+
